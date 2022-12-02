1c1c846cad847fe59688402f3bb4b4c0306e5a6e
Pizzaandy <61724429+Mega-Baboon@users.noreply.github.com>
2022-12-02 14:56:30 -0600

    moved stuff

--- day01.py
+++ day01.py
@@ -1,4 +1,4 @@
-inventories = open("input.txt").read().split("\n\n")
+inventories = open("inputs/day01.txt").read().split("\n\n")
 totals = [sum(map(int, inventory.splitlines())) for inventory in inventories]
 print("Part 1:", max(totals))
 print("Part 2:", sum(sorted(totals)[-3:]))
---
+++ day02.py
@@ -0,0 +1,13 @@
+# 0 - rock, 1 - paper, 2 - scissors
+input = open("inputs/day02.txt").read().split("\n")
+games = [("ABC".index(line[0]), "XYZ".index(line[2])) for line in input]
+
+def score(h1, h2):
+    if (h1 + 1) % 3 == h2:
+        return h2 + 1 + 6
+    elif h1 == h2:
+        return h2 + 1 + 3
+    return h2 + 1
+
+print("Part 1:", sum(score(h1, h2) for h1, h2 in games))
+print("Part 2:", sum(score(h1, (h1 + h2 - 1) % 3) for h1, h2 in games))
