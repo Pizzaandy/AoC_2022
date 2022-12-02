9dc4861823ea69c77dafc02b6d7d6f9ff327cf12
Pizzaandy <61724429+Mega-Baboon@users.noreply.github.com>
2022-12-02 10:49:41 -0600

    Update day2.py

--- Day2_RockPaperScissors/day2.py
+++ Day2_RockPaperScissors/day2.py
@@ -1,5 +1,3 @@
-from functools import reduce
-
 # 0 - rock, 1 - paper, 2 - scissors
 input = open("input.txt").read().split("\n")
 games = [("ABC".index(line[0]), "XYZ".index(line[2])) for line in input]
@@ -14,12 +12,11 @@ for opponent_hand, my_hand in games:
 print("Part 1:", score)
 
 score = 0
-outcomes = [
-    lambda h: ((h + 2) % 3),
-    lambda h: h + 3,
-    lambda h: ((h + 1) % 3) + 6,
+score_calc = [
+    lambda h: ((h + 2) % 3), # lose
+    lambda h: h + 3, # draw
+    lambda h: ((h + 1) % 3) + 6, # win
 ]
-
 for opponent_hand, result in games:
-    score += outcomes[result](opponent_hand) + 1
+    score += score_calc[result](opponent_hand) + 1
 print("Part 2:", score)
