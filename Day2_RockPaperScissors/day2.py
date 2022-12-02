dde2c291f3f2036057425986f998396ccb8b902f
Pizzaandy <61724429+Mega-Baboon@users.noreply.github.com>
2022-12-02 10:39:39 -0600

    day2

--- Day2_RockPaperScissors/day2.py
+++ Day2_RockPaperScissors/day2.py
@@ -1,46 +1,25 @@
+from functools import reduce
+
 # 0 - rock, 1 - paper, 2 - scissors
-char_to_hand = {
-    "A": "rock", "B": "paper", "C": "scissors",
-    "X": "rock", "Y": "paper", "Z": "scissors",
-}
-loses_against = {
-    "rock": "scissors",
-    "paper": "rock",
-    "scissors": "paper",
-}
-hand_to_score = {
-    "rock": 1, "paper": 2, "scissors": 3,
-}
+input = open("input.txt").read().split("\n")
+games = [("ABC".index(line[0]), "XYZ".index(line[2])) for line in input]
 
-games = open("input.txt").read().split("\n")
-
-throws = [("ABC".index(game[0]), "XYZ".index(game[2])) for game in games]
 score = 0
-for throw in throws:
-    score += throw[0] + 1
-    if throw[0] == throw[1] + 1 % 2:
+for opponent_hand, my_hand in games:
+    score += my_hand + 1
+    if (opponent_hand + 1) % 3 == my_hand:
         score += 6
-    elif throw[0] == throw[1]:
+    elif opponent_hand == my_hand:
         score += 3
-
 print("Part 1:", score)
 
 score = 0
-for game in games:
-    opponent_throw, player_throw = game.split()
-    opponent_hand = char_to_hand[opponent_throw]
+outcomes = [
+    lambda h: ((h + 2) % 3),
+    lambda h: h + 3,
+    lambda h: ((h + 1) % 3) + 6,
+]
 
-    if player_throw == "X":
-        player_hand = loses_against[opponent_hand]
-    elif player_throw == "Y":
-        player_hand = opponent_hand
-    elif player_throw == "Z":
-        player_hand = loses_against[loses_against[opponent_hand]]
-
-    score += hand_to_score[player_hand]
-    if opponent_hand == loses_against[player_hand]:
-        score += 6
-    elif opponent_hand == player_hand:
-        score += 3
-
+for opponent_hand, result in games:
+    score += outcomes[result](opponent_hand) + 1
 print("Part 2:", score)
