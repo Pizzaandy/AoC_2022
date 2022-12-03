88ea151224aa7d3a4b765441dc9015ffe10c5b59
Pizzaandy <61724429+Mega-Baboon@users.noreply.github.com>
2022-12-03 02:01:31 -0600

    Update day03.py

--- day03.py
+++ day03.py
@@ -1,10 +1,7 @@
-from itertools import zip_longest
 sacks = open("inputs/day03.txt").read().split("\n")
 
 def priority(char: str):
-    if char.islower():
-        return ord(char) - 96
-    return ord(char) - 38
+    return (ord(char) - 96) % 58
 
 data = ["".join(set(s[:len(s)//2]) & set(s[len(s)//2:])) for s in sacks]
 print("Part 1:", sum(priority(c) for c in data))
