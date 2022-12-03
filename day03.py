cd307d41816ed2133ea9cb8c96573ac3103070f5
Pizzaandy <61724429+Mega-Baboon@users.noreply.github.com>
2022-12-03 11:10:47 -0600

    Update day03.py

--- day03.py
+++ day03.py
@@ -3,9 +3,9 @@ sacks = open("inputs/day03.txt").read().split("\n")
 def priority(char: str):
     return (ord(char) - 96) % 58
 
-data = ["".join(set(s[:len(s)//2]) & set(s[len(s)//2:])) for s in sacks]
-print("Part 1:", sum(priority(c) for c in data))
+data = [set(s[:len(s)//2]) & set(s[len(s)//2:]) for s in sacks]
+print("Part 1:", sum(priority("".join(c)) for c in data))
 
 groups = zip(*3*[iter(sacks)])
-data = ["".join(set.intersection(*map(set, group))) for group in groups]
-print("Part 2:", sum(priority(c) for c in data))
+data = [set.intersection(*map(set, group)) for group in groups]
+print("Part 2:", sum(priority("".join(c)) for c in data))
