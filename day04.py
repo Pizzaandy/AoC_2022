204d5f489a90a021149df842694413d6c580e394
Pizzaandy <61724429+Mega-Baboon@users.noreply.github.com>
2022-12-04 00:36:23 -0600

    Update day04.py

--- day04.py
+++ day04.py
@@ -1,6 +1,5 @@
 input = open("inputs/day04.txt").read().splitlines()
-ranges = [line.replace(",", "-").split("-") for line in input]
-ranges = [list(map(int, r)) for r in ranges]
+data = [[*map(int, line.replace(",", "-").split("-"))] for line in input]
 
 def contains(r):
     a1, a2, b1, b2 = r
@@ -8,10 +7,10 @@ def contains(r):
     b_contains = (b1 <= a1 and b2 >= a2)
     return a_contains or b_contains
 
-print("Part 1:", sum(map(contains, ranges)))
+print("Part 1:", sum(map(contains, data)))
 
 def overlaps(r):
     a1, a2, b1, b2 = r
     return a1 <= b2 and b1 <= a2
 
-print("Part 2:", sum(map(overlaps, ranges)))
+print("Part 2:", sum(map(overlaps, data)))
