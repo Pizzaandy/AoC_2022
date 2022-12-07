4484adf5b4c3b5af1b2cf01bdb7e0f48e680838b
Pizzaandy <61724429+Mega-Baboon@users.noreply.github.com>
2022-12-07 14:25:55 -0600

    day 6 but now it's harder to read

--- day06.py
+++ day06.py
@@ -1,9 +1,7 @@
 buffer = open("inputs/day06.txt").read()
 
 def find_marker(data, n):
-    for i in range(len(data)):
-        if len(set(data[i:i+n])) == n:
-            return i + n
+    return [i+n for i in range(len(data)) if len(set(data[i:i+n])) == n][0]
 
 print("Part 1:", find_marker(buffer, 4))
 print("Part 2:", find_marker(buffer, 14))
