e4ec2af9b4552661c1ab7fe93ed974baa93f4ca1
Pizzaandy <61724429+Mega-Baboon@users.noreply.github.com>
2022-12-10 12:21:11 -0600

    Update day10.py

--- day10.py
+++ day10.py
@@ -1,28 +1,24 @@
 ops = open("inputs/day10.txt").read().splitlines()
 
 X = 1
-cycle_count = 0
-signal_strengths = []
-pixels = ["." for _ in range(240)]
+cycles = 0
+signals = []
+pixels = 240 * ["."]
 
 for op in ops:
-    if op == "noop":
-        cycle_count += 1
-        if cycle_count in [20 + n * 40 for n in range(6)]:
-            signal_strengths.append((X, cycle_count))
-        if abs(X - ((cycle_count-1) % 40)) <= 1:
-            pixels[cycle_count - 1] = "#"
-        continue
-    cmd, value = op.split()
-    if cmd == "addx":
-        for i in range(2):
-            cycle_count += 1
-            if cycle_count in [20 + n*40 for n in range(6)]:
-                signal_strengths.append(X * cycle_count)
-            if abs(X - ((cycle_count-1) % 40)) <= 1:
-                pixels[cycle_count - 1] = "#"
-            if i == 1:
-                X += int(value)
+    cycles_required = 2 if "addx" in op else 1
+    for i in range(cycles_required):
+        cycles += 1
+        if (cycles - 20) % 40 == 0:
+            signals.append(X * cycles)
+        sprite_x = (cycles - 1) % 40
+        if sprite_x - 1 <= X <= sprite_x + 1:
+            pixels[cycles - 1] = "█"
+        if i == 1:
+            X += int(op.split()[1])
 
+print("Part 1:", sum(signals))
+print("Part 2:")
 for line in zip(*[iter(pixels)]*40):
     print("".join(line))
+
