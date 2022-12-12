abfc6e7ca2df618f4b84dcbf1bb6854dd27e2a97
Pizzaandy <61724429+Mega-Baboon@users.noreply.github.com>
2022-12-12 16:48:53 -0600

    Update day12.py

--- day12.py
+++ day12.py
@@ -9,7 +9,7 @@ grid[grid == "S"] = "a"
 grid[grid == "E"] = "z"
 
 def heuristic(a, b):
-    return sum(abs(v1-v2) for v1, v2 in zip(a,b))
+    return sum(abs(v1 - v2) for v1, v2 in zip(a,b))
 
 def astar(array, start, goal):
     neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0)]
@@ -31,17 +31,15 @@ def astar(array, start, goal):
         closed.add(current)
         for i, j in neighbors:
             neighbor = current[0] + i, current[1] + j
-            gscore = gscores[current] + heuristic(current, neighbor)
             if not (0 <= neighbor[0] < array.shape[0]):
                 continue
             if not (0 <= neighbor[1] < array.shape[1]):
                 continue
-            current_c = array[current[0], current[1]]
-            neighbor_c = array[neighbor[0], neighbor[1]]
-            if ord(neighbor_c) - ord(current_c) > 1:
+            if ord(array[neighbor]) - ord(array[current]) > 1:
                 continue
-            if (neighbor in closed and
-                    gscore >= gscores.get(neighbor, 0)):
+
+            gscore = gscores[current] + heuristic(current, neighbor)
+            if neighbor in closed and gscore >= gscores.get(neighbor, 0):
                 continue
             if (gscore < gscores.get(neighbor, 0) or
                     neighbor not in [i[1] for i in oheap]):
@@ -50,10 +48,9 @@ def astar(array, start, goal):
                 fscores[neighbor] = gscore + heuristic(neighbor, goal)
                 heapq.heappush(oheap, (fscores[neighbor], neighbor))
 
-route = astar(grid, start, goal)
-
 all_a = [(int(x), int(y)) for x, y in np.dstack(np.where(grid == "a"))[0]]
 potential_routes = [res for a in all_a if (res := astar(grid, a, goal))]
 potential_routes.sort(key = len)
-print("Part 1:", len(route))
+
+print("Part 1:", len(astar(grid, start, goal)))
 print("Part 2:", len(potential_routes[0]))
